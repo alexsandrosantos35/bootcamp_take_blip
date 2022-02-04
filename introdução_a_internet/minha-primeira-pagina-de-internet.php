ebc158034157fdf198903524a2ad0e0666efd97e
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-02-03 22:52:11 -0300

    algumas alteraçöes

---
+++ introdução_a_internet/minha-primeira-pagina-de-internet.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+< meta lang="pt_br">
+<meta charset="utf8">
+<title>Minha primeira página php</title>
+
+</head>
+<body>
+  <h1>Script php em ação</h1>
+  
+  <?php  
+          for ($i = 0; $i<10; $i++)  {
+            echo "Página número ".i. "<br />";
+          }
+          ?>
+</body> 
+</html>
